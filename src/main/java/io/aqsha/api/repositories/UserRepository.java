deeb2a23c2b5d61873f6f2a9a960665d81dcc302
Julien <julien@rbrt.fr>
2019-07-10 13:14:36 +0200

    Add entities

---
+++ src/main/java/io/aqsha/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package io.aqsha.api.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import io.aqsha.api.models.User;;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
